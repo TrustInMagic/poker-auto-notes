9869f9196f44529ae7fd9a896310fddc2a922f31
Teotim Ionescu <timionescu@gmail.com>
2024-01-11 21:17:20 +0200

    Add player cards to table and style

---
+++ src/components/Player/Player.tsx
@@ -0,0 +1,66 @@
+'use client';
+
+import React from 'react';
+import style from './Player.module.css';
+import { player1, player2, player3, player4, player5 } from './player.config';
+
+interface PlayerProps {
+  position: number;
+}
+
+function Player({ position }: PlayerProps) {
+  const [name, setName] = React.useState('');
+  const [nameSaved, setNameSaved] = React.useState(false);
+
+  let customStyle;
+
+  switch (position) {
+    case 1:
+      customStyle = player1;
+      break;
+    case 2:
+      customStyle = player2;
+      break;
+    case 3:
+      customStyle = player3;
+      break;
+    case 4:
+      customStyle = player4;
+      break;
+    case 5:
+      customStyle = player5;
+  }
+
+  return (
+    <div className={style.player} style={customStyle}>
+      {!nameSaved ? (
+        <div className={style.setter}>
+          <input
+            className={style['name-input']}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button
+            className={style['set-name']}
+            onClick={() => setNameSaved(true)}
+          >
+            Set name
+          </button>
+        </div>
+      ) : (
+        <div className={style['name-set']}>
+          <div className={style.name}>{name}</div>
+          <button
+            className={style['edit-name']}
+            onClick={() => {
+              setNameSaved(false);
+            }}
+          >
+            Edit name
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Player;
---
+++ src/components/Player/player.config.ts
@@ -0,0 +1,11 @@
+const player1 = { left: '4%', top: '70%' };
+
+const player2 = { left: '1%', top: '47%' };
+
+const player3 = { left: '5%', top: '20%' };
+
+const player4 = { left: '30%', top: '6%' };
+
+const player5 = { left: '60%', top: '6%' };
+
+export { player1, player2, player3, player4, player5 };
