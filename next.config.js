a72f2e7fce757965ecbcb1e79592d3e52549b2fe
Teotim Ionescu <timionescu@gmail.com>
2024-08-27 18:34:54 +0300

    Add assetPrefix to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: '/poker-auto-notes',
+  assetPrefix: '/poker-auto-notes',
   output: 'export',
   reactStrictMode: true,
 };
